aa08deee5911b5bab7f577c4120db27310dc0998
migcintron <59234464+migcintron@users.noreply.github.com>
2023-08-31 14:59:32 -0600

    Add show databases

--- terraform/user-data-scripts/user-data-crud-db.sh
+++ terraform/user-data-scripts/user-data-crud-db.sh
@@ -5,6 +5,7 @@ yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
 # Step 2 Change Hello World for Bonjour le monde
-#mysql -u myrdsuser -p  -h myrdsinstance.ciwxmuhfadid.us-east-1.rds.amazonaws.com -P 3340
+#mysql -u myrdsuser -p  -h localhost -P 3340
 echo "Bonjour le monde from DB $(hostname -f)" > /var/www/html/index.html
-
+echo "myrdspassword" | mysql -u myrdsuser -p  -h localhost -P 3340
+echo "SHOW DATABASES"
