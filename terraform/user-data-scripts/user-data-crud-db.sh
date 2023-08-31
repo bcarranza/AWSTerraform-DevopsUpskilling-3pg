e3452fcdafe314e7d4725f08534aacb509e44b2d
migcintron <59234464+migcintron@users.noreply.github.com>
2023-08-31 15:26:16 -0600

    Update user-data-crud-db.sh

--- terraform/user-data-scripts/user-data-crud-db.sh
+++ terraform/user-data-scripts/user-data-crud-db.sh
@@ -7,5 +7,3 @@ systemctl enable httpd.service
 # Step 2 Change Hello World for Bonjour le monde
 #mysql -u myrdsuser -p  -h localhost -P 3340
 echo "Bonjour le monde from DB $(hostname -f)" > /var/www/html/index.html
-echo "myrdspassword" | mysql -u myrdsuser -p  -h localhost -P 3340
-echo "SHOW DATABASES"
