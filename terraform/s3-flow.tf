923972c9061f7ffa1da108d991278c70434cfdae
1890victor@gmail.com <victor.rodriguez@janeirodigital.com>
2023-08-31 23:11:57 -0600

    Transform the S3 flow log bucket to an encrypted bucket

--- terraform/s3-flow.tf
+++ terraform/s3-flow.tf
@@ -1,7 +1,16 @@
 # Create an AWS S3 Bucket resource
 resource "aws_s3_bucket" "this" {
   bucket = "435924829664-bc3pg-${local.prefix}-vjr-vpc-flow-log"   # Name of the S3 bucket, generated based on the provided prefix and additional text "bc-"
+  force_destroy = true # Force destroy otherwise fails
 
+  # Single object for setting server-side encryption by default.
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
   tags = {
     Name        = "${local.prefix}-vjr-vpc-flow-log"   # Tag the bucket with a descriptive name
     Environment = local.env                         # Tag the bucket with the environment
