f01b23cf78a63d2e6dd655c6e148ddfb1b7b4ec5
Art Bugorski <art.bugorski@gmail.com>
2023-08-31 21:48:45 -0400

    change Apache site response from "Hello World" to "Bonjour le monde"

--- terraform/user-data-scripts/user-data-crud-db.sh
+++ terraform/user-data-scripts/user-data-crud-db.sh
@@ -4,6 +4,4 @@ yum update -y
 yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
-echo "Hello World from DB $(hostname -f)" > /var/www/html/index.html
-
-
+echo "Bonjour le monde from DB $(hostname -f)" > /var/www/html/index.html # change Apache site response from "Hello World" to "Bonjour le monde"
