a74ef3cd9568e1c476a0b2a5a7253cb206aa01b8
DiagonalJ117 <americansk8land@gmail.com>
2023-09-05 20:11:57 -0700

    encrypted s3 bucket and changed resource name

--- terraform/s3-flow.tf
+++ terraform/s3-flow.tf
@@ -1,9 +1,18 @@
 # Create an AWS S3 Bucket resource
 resource "aws_s3_bucket" "this" {
-  bucket = "435924829664-bc3pg-${local.prefix}-vpc-flow-log"   # Name of the S3 bucket, generated based on the provided prefix and additional text "bc-"
+  bucket = "435924829664-bc3pg-${local.prefix}-diagonalj117-vpc-flow-log"   # Name of the S3 bucket, generated based on the provided prefix and additional text "bc-"
+
+  # Setting server-side bucket encryption
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 
   tags = {
-    Name        = "${local.prefix}-vpc-flow-log"   # Tag the bucket with a descriptive name
+    Name        = "${local.prefix}-diagonalj117-vpc-flow-log"   # Tag the bucket with a descriptive name
     Environment = local.env                         # Tag the bucket with the environment
     Path        = "${basename(abspath(path.module))}/s3-flow.tf"   # Tag the bucket with the configuration file path
   }
